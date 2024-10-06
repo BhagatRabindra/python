14ce6e859157cc83200ed8287b158c6c028efe85
Rabindra <rbnbhagat00006@gmail.com>
2024-10-06 22:27:01 +0530

    commented elif.py file

--- elif.py
+++ elif.py
@@ -1,12 +1,12 @@
-light = input(" Write the name of color: ")
-#hello this is rabindra
-if(light =="red"):
-    print("Stop")
-elif(light == "yellow "):
-    print("Look")
-elif(light == "green"):
-    print("Go")
+# light = input(" Write the name of color: ")
+# #hello this is rabindra
+# if(light =="red"):
+#     print("Stop")
+# elif(light == "yellow "):
+#     print("Look")
+# elif(light == "green"):
+#     print("Go")
 
-else:
-    print("Something is Wrong")
+# else:
+#     print("Something is Wrong")
 
